75b24ea34947773123f2251f9bbd7dbf62912bb0
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-11-15 18:09:16 +0200

    update: change program's code

--- test_task/program.py
+++ test_task/program.py
@@ -13,12 +13,12 @@ def shapes_program():
 
     user_input = input("Enter shape and parameters: ")
     user_input = user_input.split()
-    shape = user_input[0].capitalize()
+    user_shape = user_input[0].capitalize()
     numbers = []
     perimeter = 0
     area = 0
 
-    if shape.capitalize() in SHAPES:
+    if user_shape.capitalize() in SHAPES:
         for number in user_input:
             try:
                 number = float(number)
@@ -29,24 +29,23 @@ def shapes_program():
         return "We don't have this shape"
         
     
-    if shape == SHAPES[0] and len(numbers) == 3:
-        square = Square((numbers[0], numbers[1]), numbers[2])
-        if square.side < 0:
+    if user_shape == SHAPES[0] and len(numbers) == 3:
+        shape = Square((numbers[0], numbers[1]), numbers[2])
+        if shape.side < 0:
             return "The side of a squere cannot be negative"
-        perimeter = square.perimeter()
-        area = square.area()
-    elif shape == SHAPES[1] and len(numbers) == 4:
-        rectangle = Rectangle((numbers[0], numbers[1]), (numbers[2], numbers[3]))
-        perimeter = rectangle.perimeter()
-        area = rectangle.area()
-    elif shape == SHAPES[2] and len(numbers) == 3:
-        circle = Circle((numbers[0], numbers[1]), numbers[2])
-        if circle.radius < 0:
+        
+    elif user_shape == SHAPES[1] and len(numbers) == 4:
+        shape = Rectangle((numbers[0], numbers[1]), (numbers[2], numbers[3]))
+
+    elif user_shape == SHAPES[2] and len(numbers) == 3:
+        shape = Circle((numbers[0], numbers[1]), numbers[2])
+        if shape.radius < 0:
             return "The radius of a circle cannot be negative"
-        perimeter = circle.perimeter()
-        area = circle.area()
+
     else:
         return "Invalid number of parameters for the shape."
+    perimeter = shape.perimeter()
+    area = shape.area()
     
-    return f"{shape} Perimeter {perimeter}, Area {area}"
+    return f"{user_shape} Perimeter {perimeter}, Area {area}"
         
